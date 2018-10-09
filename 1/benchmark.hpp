0b46eb0b006a96f97db88dc12b350b9fa3bc92a6
Davide Pino Russo <dvd.rss.pn@gmail.com>
2018-10-09 21:11:52 +0200

    Added first homework

---
+++ 1/benchmark.hpp
@@ -0,0 +1,23 @@
+#ifndef _BENCHMARK_H_
+#define _BENCHMARK_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
+#include <fstream>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/random.h>
+
+using namespace std;
+
+#define NUM_ESPERIMENTI 30
+#define N 4
+
+void initBuffer(long int);
+void termBuffer(void);
+void analysisWrite(long int, long int);
+void analysisRead(long int, long int);
+
+#endif //_BENCHMARK_H_
